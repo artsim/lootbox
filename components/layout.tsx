49839624c24dcc42f499d773c91bbacf6bc6039b
Arthur Simiyu <hello@arthursimiyu.com>
2022-01-30 00:59:37 +0300

    Display notification when user recives a pack

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import Footer from "./footer";
 import Navbar from "./navbar";
+import usePackEvents from "../hooks/usePackEvents";
 
 type Props = {
-  title: string;
+    title: string;
 };
 
 export default function Layout({
-  children,
-  title,
+    children,
+    title,
 }: React.PropsWithChildren<Props>) {
-  return (
-    <>
-      <Navbar />
-      <main className="max-w-3xl mx-auto my-4">
-        <div className="flex flex-col gap-8">
-          <h1 className="text-6xl font-bold text-blue-600">{title}</h1>
-          {children}
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
+    usePackEvents();
+    return (
+        <>
+            <Navbar />
+            <main className="max-w-3xl mx-auto my-4">
+                <div className="flex flex-col gap-8">
+                    <h1 className="text-6xl font-bold text-blue-600">{title}</h1>
+                    {children}
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
 }
